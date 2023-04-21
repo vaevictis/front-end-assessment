9c30b809db9bc34019a8534f987eed8aefe25ba7
Guillaume Hammadi <ghammadi@gmail.com>
2023-04-21 13:41:46 -0700

    Basic design fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { GistsComponent } from './components/gistsComponent'
 
@@ -8,7 +7,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1>Gist Finder</h1>
       </header>
       <GistsComponent userName={'gaearon'} />
     </div>
