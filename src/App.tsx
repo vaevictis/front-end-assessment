fe5002ca78c23fed90396bae96240ba589da0c11
Guillaume Hammadi <ghammadi@gmail.com>
2023-04-20 19:21:47 -0700

    Simple refactor to declare component props type

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
-      <GistsComponent />
+      <GistsComponent userName={'gaearon'} />
     </div>
   );
 }
--- src/components/gistsComponent.tsx
+++ src/components/gistsComponent.tsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Octokit } from 'octokit'
 
-export function GistsComponent(userName: string) {
+interface UserName {
+  userName: string
+}
+
+export function GistsComponent(props:UserName) {
   const [data, setData] = useState<object | null>(null)
-  const userName: String = 'gaearon'
+  const { userName } = props
+
   const octokit = new Octokit({
     auth: process.env.REACT_APP_GITHUB_TOKEN
   })
