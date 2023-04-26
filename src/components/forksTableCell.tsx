2b59f2f0c211103b9bf2df8322644ea34a795df1
Guillaume Hammadi <ghammadi@gmail.com>
2023-04-25 18:49:57 -0700

    Refactor Avatar into Forker

--- src/components/forksTableCell.tsx
+++ src/components/forksTableCell.tsx
@@ -8,7 +8,7 @@ const octokit = new Octokit({
   auth: process.env.REACT_APP_GITHUB_TOKEN
 })
 
-type Avatar = {
+type Forker = {
   forkUrl: string,
   avatarUrl: string,
   login: string
@@ -17,9 +17,15 @@ type Avatar = {
 const ForksTableCell = (props:any): JSX.Element => {
   const { forksUrl } = props
   const [forks, setForks] = useState<object[] | null>(null)
-  const [avatars, setAvatars] = useState<Avatar[]>([])
+  const [forkers, setForkers] = useState<Forker[]>([])
+
+  // FIXME: This would quickly become a bottleneck.
+  // I end up looping and making an extra API request for every gist
+  // after I've unlocked the paginated gists request.
+  // This is a clear n+1 problem.
 
   useEffect(() => {
+    // TODO: migrate to the integrated Octokit syntax
     const fetchData = async ():Promise<any> => {
       const result = await octokit.request(`GET ${forksUrl}`, {
         forksUrl: forksUrl,
@@ -37,35 +43,34 @@ const ForksTableCell = (props:any): JSX.Element => {
   useEffect(() => {
     if (!forks) {return}
 
-    extractAvatarData(forks)
+    extractForkerData(forks)
   }, [forks])
 
-  const extractAvatarData = (forks:any[]) :void => {
-    const avatarData: Avatar[] = []
-    // debugger
+  const extractForkerData = (forks:any[]) :void => {
+    const forkerData: Forker[] = []
     forks.forEach((fork:any) :void => {
-      avatarData.push({
+      forkerData.push({
         avatarUrl: fork.owner.avatar_url,
         forkUrl: fork.html_url,
         login: fork.owner.login
       })
     })
 
-    setAvatars(avatarData)
+    setForkers(forkerData)
   }
-  if (!avatars) {
+  if (!forkers) {
     return (<TableCell align="left">No Forks yet</TableCell>)
   }
 
   return (
     <TableCell align="left">
       <Stack>
-        {avatars.map((avatar, idx) => (
-          <span key={`avatar_${idx}`}>
-            <a href={avatar.forkUrl} target="_blank">
-              <img className="avatar" src={avatar.avatarUrl} />
+        {forkers.map((forker, idx) => (
+          <span key={`forker_${idx}`}>
+            <a href={forker.forkUrl} target="_blank">
+              <img className="avatar" src={forker.avatarUrl} />
             </a>
-            <span> {avatar.login}</span>
+            <span> {forker.login}</span>
           </span>
         ))}
       </Stack>
